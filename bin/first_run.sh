f75452e186815e475b2e92564726eb824d263271
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-04-22 12:17:17 -0400

    Changed docker MONGO variable

--- bin/first_run.sh
+++ bin/first_run.sh
@@ -4,10 +4,10 @@ while [ `opal rest -o https://localhost:8443 -u administrator -p password -m GET
 do
     echo -n "."
     sleep 5
-    sed s/@mongo_host@/$MONGODB_PORT_27017_TCP_ADDR/g /opt/opal/data/idsdb.json | \
-        sed s/@mongo_port@/$MONGODB_PORT_27017_TCP_PORT/g | \
+    sed s/@mongo_host@/$MONGO_PORT_27017_TCP_ADDR/g /opt/opal/data/idsdb.json | \
+        sed s/@mongo_port@/$MONGO_PORT_27017_TCP_PORT/g | \
         opal rest -o https://localhost:8443 -u administrator -p password -m POST /system/databases --content-type "application/json"
-    sed s/@mongo_host@/$MONGODB_PORT_27017_TCP_ADDR/g /opt/opal/data/mongodb.json | \
-        sed s/@mongo_port@/$MONGODB_PORT_27017_TCP_PORT/g | \
+    sed s/@mongo_host@/$MONGO_PORT_27017_TCP_ADDR/g /opt/opal/data/mongodb.json | \
+        sed s/@mongo_port@/$MONGO_PORT_27017_TCP_PORT/g | \
         opal rest -o https://localhost:8443 -u administrator -p password -m POST /system/databases --content-type "application/json"
 done
