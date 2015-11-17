93819e7b9a0e0af93219c8e959dbce564b533287
Yannick Marcon <yannick.marcon@obiba.org>
2015-11-17 14:06:54 -0500

    sleep while waiting for opal

--- bin/first_run.sh
+++ bin/first_run.sh
@@ -4,7 +4,6 @@
 echo "Initializing Opal databases..."
 while [ `opal rest -o https://localhost:8443 -u administrator -p password -m GET /system/databases | grep -ch "mongodb"` -eq 0 ]
 do
-    echo -n "."
     sleep 5
     sed s/@mongo_host@/$MONGO_PORT_27017_TCP_ADDR/g /opt/opal/data/idsdb.json | \
         sed s/@mongo_port@/$MONGO_PORT_27017_TCP_PORT/g | \
