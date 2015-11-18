e34822bf06f1f493ed6afdbfb02c7a7d82c02151
Yannick Marcon <yannick.marcon@obiba.org>
2015-11-18 17:08:46 -0500

    rserver docker linkage added

--- bin/start.sh
+++ bin/start.sh
@@ -6,8 +6,15 @@ if [ -n "$AGATE_PORT_8444_TCP_ADDR" -a -e /opt/opal/bin/first_run.sh ]
     sed s/localhost:8444/$AGATE_PORT_8444_TCP_ADDR:$AGATE_PORT_8444_TCP_PORT/g /etc/opal/opal-config.properties | \
      sed s/#org.obiba.realm.url/org.obiba.realm.url/g > /tmp/opal-config.properties
 	mv -f /tmp/opal-config.properties /etc/opal/opal-config.properties
-	chown -R opal:adm /etc/opal
 fi
+
+if [ -n "$RSERVER_PORT_6312_TCP_ADDR" -a -e /opt/opal/bin/first_run.sh ]
+    then
+    sed s/#org.obiba.opal.Rserve.host=/org.obiba.opal.Rserve.host=$RSERVER_PORT_6312_TCP_ADDR/g /etc/opal/opal-config.properties > /tmp/opal-config.properties
+	mv -f /tmp/opal-config.properties /etc/opal/opal-config.properties
+fi
+
+chown -R opal:adm /etc/opal
 
 # Start opal as a service
 service opal start
