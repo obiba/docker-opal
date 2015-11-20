c28823d39c9377326a43798057417992ce73afba
Yannick Marcon <yannick.marcon@obiba.org>
2015-11-20 13:15:21 -0500

    Install datashield packages

--- bin/first_run.sh
+++ bin/first_run.sh
@@ -6,6 +6,7 @@ until opal rest -o https://localhost:8443 -u administrator -p password -m GET /s
 do
 	sleep 5
 done
+
 echo "Initializing Opal databases..."
 sed s/@mongo_host@/$MONGO_PORT_27017_TCP_ADDR/g /opt/opal/data/idsdb.json | \
     sed s/@mongo_port@/$MONGO_PORT_27017_TCP_PORT/g | \
@@ -13,3 +14,9 @@ sed s/@mongo_host@/$MONGO_PORT_27017_TCP_ADDR/g /opt/opal/data/idsdb.json | \
 sed s/@mongo_host@/$MONGO_PORT_27017_TCP_ADDR/g /opt/opal/data/mongodb.json | \
     sed s/@mongo_port@/$MONGO_PORT_27017_TCP_PORT/g | \
     opal rest -o https://localhost:8443 -u administrator -p password -m POST /system/databases --content-type "application/json"
+
+echo "Initializing Datashield..."
+if [ -n "$RSERVER_PORT_6312_TCP_ADDR" ]
+	then
+	opal rest -o https://localhost:8443 -u administrator -p password -m POST /datashield/packages?name=datashield
+fi
