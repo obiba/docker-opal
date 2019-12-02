7ca2f1c46434f051141720f12111121cc89a97a0
Yannick Marcon <yannick.marcon@obiba.org>
2019-12-02 12:29:17 +0000

    resources docker scripts

--- resources/bin/first_run.sh
+++ resources/bin/first_run.sh
@@ -1,21 +1,61 @@
 #!/bin/bash
 
 # Configure some databases for IDs and data
+
+# Legacy parameters
 if [ -n "$MONGO_PORT_27017_TCP_ADDR" ]
+then
+	MONGO_HOST=$MONGO_PORT_27017_TCP_ADDR
+fi
+if [ -n "$MONGO_PORT_27017_TCP_PORT" ]
+then
+        MONGO_PORT=$MONGO_PORT_27017_TCP_PORT
+fi
+if [ -n "$MYSQLIDS_PORT_3306_TCP_ADDR" ]
+then
+        MYSQLIDS_HOST=$MYSQLIDS_PORT_3306_TCP_ADDR
+fi
+if [ -n "$MYSQLIDS_PORT_3306_TCP_PORT" ]
+then
+        MYSQLIDS_PORT=$MYSQLIDS_PORT_3306_TCP_PORT
+fi
+if [ -n "$MYSQLDATA_PORT_3306_TCP_ADDR" ]
+then
+        MYSQLDATA_HOST=$MYSQLDATA_PORT_3306_TCP_ADDR
+fi
+if [ -n "$MYSQLDATA_PORT_3306_TCP_PORT" ]
+then
+        MYSQLDATA_PORT=$MYSQLDATA_PORT_3306_TCP_PORT
+fi
+
+#
+# MongoDB
+#
+
+if [ -n "$MONGO_HOST" ]
+then
+	if [ -z "$MONGO_PORT" ]
 	then
-	echo "Initializing Opal databases with MongoDB..."
-	if [ -z "$MYSQLIDS_PORT_3306_TCP_ADDR" ]
+		MONGO_PORT=27017
+	fi
+	if [ -z "$MYSQLIDS_HOST" ]
 		then
-		sed s/@mongo_host@/$MONGO_PORT_27017_TCP_ADDR/g /opt/opal/data/mongodb-ids.json | \
-    		sed s/@mongo_port@/$MONGO_PORT_27017_TCP_PORT/g | \
+		echo "Initializing Opal IDs database with MongoDB..."
+		sed s/@mongo_host@/$MONGO_HOST/g /opt/opal/data/mongodb-ids.json | \
+    		sed s/@mongo_port@/$MONGO_PORT/g | \
     		opal rest -o https://localhost:8443 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m POST /system/databases --content-type "application/json"
-    fi
-	sed s/@mongo_host@/$MONGO_PORT_27017_TCP_ADDR/g /opt/opal/data/mongodb-data.json | \
-    	sed s/@mongo_port@/$MONGO_PORT_27017_TCP_PORT/g | \
+	fi
+	echo "Initializing Opal data database with MongoDB..."
+	sed s/@mongo_host@/$MONGO_HOST/g /opt/opal/data/mongodb-data.json | \
+    	sed s/@mongo_port@/$MONGO_PORT/g | \
     	opal rest -o https://localhost:8443 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m POST /system/databases --content-type "application/json"
 fi
 
-if [ -n "$MYSQLIDS_PORT_3306_TCP_ADDR" ]
+#
+# MySQL (TODO MariaDB support)
+#
+
+if [ -n "$MYSQLIDS_HOST" ]
 	then
 	echo "Initializing Opal IDs database with MySQL..."
 
@@ -31,15 +71,15 @@ if [ -n "$MYSQLIDS_PORT_3306_TCP_ADDR" ]
 		MID_USER=$MYSQLIDS_USER
 	fi
 
-	sed s/@mysql_host@/$MYSQLIDS_PORT_3306_TCP_ADDR/g /opt/opal/data/mysqldb-ids.json | \
-    	sed s/@mysql_port@/$MYSQLIDS_PORT_3306_TCP_PORT/g | \
+	sed s/@mysql_host@/$MYSQLIDS_HOST/g /opt/opal/data/mysqldb-ids.json | \
+    	sed s/@mysql_port@/$MYSQLIDS_PORT/g | \
     	sed s/@mysql_db@/$MID_DB/g | \
     	sed s/@mysql_user@/$MID_USER/g | \
     	sed s/@mysql_pwd@/$MYSQLIDS_PASSWORD/g | \
     	opal rest -o https://localhost:8443 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m POST /system/databases --content-type "application/json"
 fi
 
-if [ -n "$MYSQLDATA_PORT_3306_TCP_ADDR" ]
+if [ -n "$MYSQLDATA_HOST" ]
 	then
 	echo "Initializing Opal data database with MySQL..."
 
@@ -56,13 +96,13 @@ if [ -n "$MYSQLDATA_PORT_3306_TCP_ADDR" ]
 	fi
 
 	MD_DEFAULT="false"
-	if [ -z "$MONGO_PORT_27017_TCP_ADDR" ]
+	if [ -z "$MONGO_HOST" ]
 		then
 		MD_DEFAULT="true"
 	fi
 
-	sed s/@mysql_host@/$MYSQLDATA_PORT_3306_TCP_ADDR/g /opt/opal/data/mysqldb-data.json | \
-    	sed s/@mysql_port@/$MYSQLDATA_PORT_3306_TCP_PORT/g | \
+	sed s/@mysql_host@/$MYSQLDATA_HOST/g /opt/opal/data/mysqldb-data.json | \
+    	sed s/@mysql_port@/$MYSQLDATA_PORT/g | \
     	sed s/@mysql_db@/$MD_DB/g | \
     	sed s/@mysql_user@/$MD_USER/g | \
     	sed s/@mysql_pwd@/$MYSQLDATA_PASSWORD/g | \
@@ -71,8 +111,10 @@ if [ -n "$MYSQLDATA_PORT_3306_TCP_ADDR" ]
 fi
 
 # Configure datashield packages
-if [ -n "$RSERVER_PORT_6312_TCP_ADDR" ]
+if [ -n "$RSERVER_HOST" ]
 	then
 	echo "Initializing Datashield..."
 	opal rest -o https://localhost:8443 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m POST /datashield/packages?name=datashield
+	opal rest -o https://localhost:8443 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m POST "/service/r/packages?name=obiba%2Fresourcer&ref=master"
 fi
+
--- resources/bin/start.sh
+++ resources/bin/start.sh
@@ -1,49 +1,92 @@
 #!/bin/bash
 
+# Legacy parameters
+if [ -n "$AGATE_PORT_8444_TCP_ADDR" ]
+then
+        AGATE_HOST=$AGATE_PORT_8444_TCP_ADDR
+fi
+if [ -n "$AGATE_PORT_8444_TCP_PORT" ]
+then
+        AGATE_PORT=$AGATE_PORT_8444_TCP_PORT
+fi
+if [ -n "$RSERVER_PORT_6312_TCP_ADDR" ]
+then
+        RSERVER_HOST=$RSERVER_PORT_6312_TCP_ADDR
+fi
+
 # Make sure conf folder is available
 if [ ! -d $OPAL_HOME/conf ]
-    then
-    mkdir -p $OPAL_HOME/conf
-    cp -r /usr/share/opal/conf/* $OPAL_HOME/conf
+then
+	echo "Preparing default conf in $OPAL_HOME ..."
+	mkdir -p $OPAL_HOME/conf
+	cp -r /usr/share/opal/conf/* $OPAL_HOME/conf
 fi
+
+# Install default plugins
+if [ ! -d $OPAL_HOME/plugins ]
+then
+        echo "Preparing default plugins in $OPAL_HOME ..."
+        mkdir -p $OPAL_HOME/plugins
+        cp -r /usr/share/opal/plugins/* $OPAL_HOME/plugins
+fi
+
+#
+# Agate
+#
 
 # check if 1st run. Then configure properties.
-if [ -n "$AGATE_PORT_8444_TCP_ADDR" -a -e /opt/opal/bin/first_run.sh ]
-    then
-    sed s/localhost:8444/$AGATE_PORT_8444_TCP_ADDR:$AGATE_PORT_8444_TCP_PORT/g $OPAL_HOME/conf/opal-config.properties | \
-     sed s/#org.obiba.realm.url/org.obiba.realm.url/g > /tmp/opal-config.properties
+if [ -n "$AGATE_HOST" -a -e /opt/opal/bin/first_run.sh ]
+then
+	echo "Setting Agate connection..."
+	if [ -z "$AGATE_PORT" ]
+	then
+		AGATE_PORT=8444
+	fi
+	sed s/localhost:8444/$AGATE_HOST:$AGATE_PORT/g $OPAL_HOME/conf/opal-config.properties | \
+		sed s/#org.obiba.realm.url/org.obiba.realm.url/g > /tmp/opal-config.properties
 	mv -f /tmp/opal-config.properties $OPAL_HOME/conf/opal-config.properties
 fi
 
-if [ -n "$RSERVER_PORT_6312_TCP_ADDR" -a -e /opt/opal/bin/first_run.sh ]
-    then
-    sed s/#org.obiba.opal.Rserve.host=/org.obiba.opal.Rserve.host=$RSERVER_PORT_6312_TCP_ADDR/g $OPAL_HOME/conf/opal-config.properties > /tmp/opal-config.properties
+#
+# R server
+#
+
+if [ -n "$RSERVER_HOST" -a -e /opt/opal/bin/first_run.sh ]
+then
+	echo "Setting R server connection..."
+	sed s/#org.obiba.opal.Rserve.host=/org.obiba.opal.Rserve.host=$RSERVER_HOST/g $OPAL_HOME/conf/opal-config.properties > /tmp/opal-config.properties
 	mv -f /tmp/opal-config.properties $OPAL_HOME/conf/opal-config.properties
 fi
+
+#
+# Administrator password
+#
 
 if [ -e /opt/opal/bin/set_password.sh ]
-    then
-    /opt/opal/bin/set_password.sh
-    mv /opt/opal/bin/set_password.sh /opt/opal/bin/set_password.sh.done
+then
+	echo "Setting password..."
+	/opt/opal/bin/set_password.sh
+	mv /opt/opal/bin/set_password.sh /opt/opal/bin/set_password.sh.done
 fi
 
 # Start opal
 if [ -e /opt/opal/bin/first_run.sh ]
-    then
-    # check if 1st run. Then configure database and datashield.
+then
+	echo "Starting Opal before first run script..."
+	# check if 1st run. Then configure database and datashield.
 	/usr/share/opal/bin/opal &
-
 	# Wait for the opal server to be up and running
 	echo "Waiting for Opal to be ready..."
 	until opal rest -o https://localhost:8443 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m GET /system/databases &> /dev/null
 	do
-	    sleep 5
+		sleep 5
 	done
-
-    /opt/opal/bin/first_run.sh
-    mv /opt/opal/bin/first_run.sh /opt/opal/bin/first_run.sh.done
-
-    tail -f $OPAL_HOME/logs/opal.log
+	echo "First run setup..."
+	/opt/opal/bin/first_run.sh
+	mv /opt/opal/bin/first_run.sh /opt/opal/bin/first_run.sh.done
+	ls /srv/plugins
+	tail -f $OPAL_HOME/logs/opal.log
 else
+	echo "Starting Opal..."
 	/usr/share/opal/bin/opal
 fi
