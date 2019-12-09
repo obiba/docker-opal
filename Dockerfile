f6174b7786f869c3cf46cf01e3c948c211b3e547
Yannick Marcon <yannick.marcon@obiba.org>
2019-12-09 20:39:29 +0100

    snapshot tag is based on master branch, docker-compose v3 and new environment variables

--- Dockerfile
+++ Dockerfile
@@ -24,13 +24,23 @@ RUN git checkout $OPAL_BRANCH; \
     mvn clean install && \
     mvn -Prelease org.apache.maven.plugins:maven-antrun-plugin:run@make-deb
 
-
 FROM openjdk:8-jdk-stretch AS server
 
 ENV OPAL_ADMINISTRATOR_PASSWORD password
 ENV OPAL_HOME /srv
+ENV OPAL_DIST /usr/share/opal
 ENV JAVA_OPTS "-Xms1G -Xmx2G -XX:MaxPermSize=256M -XX:+UseG1GC"
 
+ENV SEARCH_ES_VERSION=1.0.0
+ENV VCF_STORE_VERSION=1.0.2
+ENV SAMTOOLS_VERSION=1.4
+ENV LIMESURVEY_PLUGIN_VERSION=1.1.0
+ENV REDCAP_PLUGIN_VERSION=1.1.0
+ENV SPSS_PLUGIN_VERSION=1.1.0
+ENV READR_PLUGIN_VERSION=1.1.0
+ENV READXL_PLUGIN_VERSION=1.1.0
+ENV GOOGLESHEETS_PLUGIN_VERSION=1.0.0
+ENV VALIDATE_PLUGIN_VERSION=1.0.1
 ENV SAMTOOLS_VERSION 1.4
 ENV HTSDIR /projects/htslib
 ENV SAMDIR /projects/samtools-$SAMTOOLS_VERSION
@@ -44,11 +54,14 @@ RUN apt-get update && \
 
 COPY --from=gosu /usr/local/bin/gosu /usr/local/bin/
 
-COPY /bin /opt/opal/bin
-COPY /data /opt/opal/data
-RUN chmod +x -R /opt/opal/bin; \
-    chown -R opal /opt/opal; \
-    chmod +x /usr/share/opal/bin/opal
+# Install Opal Python Client
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https unzip
+
+RUN \
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61 && \
+    echo 'deb https://dl.bintray.com/obiba/deb all main' | tee /etc/apt/sources.list.d/obiba.list && \
+    apt-get update && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y opal-python-client
 
 # Plugins dependencies
 WORKDIR /projects
@@ -73,6 +86,52 @@ RUN make -j HTSDIR=$HTSDIR ; \
 
 RUN apt-get purge -y \
     make gcc liblzma-dev libbz2-dev libncurses5-dev zlib1g-dev
+
+WORKDIR $OPAL_DIST
+
+# Install Search ES plugin
+RUN \
+  curl -L -o $OPAL_DIST/plugins/opal-search-es-${SEARCH_ES_VERSION}-dist.zip https://github.com/obiba/opal-search-es/releases/download/${SEARCH_ES_VERSION}/opal-search-es-${SEARCH_ES_VERSION}-dist.zip
+
+# Install Jennite
+RUN \
+  curl -L -o $OPAL_DIST/plugins/jennite-vcf-store-${VCF_STORE_VERSION}-dist.zip https://github.com/obiba/jennite/releases/download/${VCF_STORE_VERSION}/jennite-vcf-store-${VCF_STORE_VERSION}-dist.zip
+
+# Install Limesurvey datasource plugin
+RUN \
+  curl -L -o $OPAL_DIST/plugins/opal-datasource-limesurvey-${LIMESURVEY_PLUGIN_VERSION}-dist.zip https://github.com/obiba/opal-datasource-limesurvey/releases/download/${LIMESURVEY_PLUGIN_VERSION}/opal-datasource-limesurvey-${LIMESURVEY_PLUGIN_VERSION}-dist.zip
+
+# Install REDCap datasource plugin
+RUN \
+  curl -L -o $OPAL_DIST/plugins/opal-datasource-redcap-${REDCAP_PLUGIN_VERSION}-dist.zip https://github.com/obiba/opal-datasource-redcap/releases/download/${REDCAP_PLUGIN_VERSION}/opal-datasource-redcap-${REDCAP_PLUGIN_VERSION}-dist.zip
+
+# Install SPSS datasource plugin
+RUN \
+  curl -L -o $OPAL_DIST/plugins/opal-datasource-spss-${SPSS_PLUGIN_VERSION}-dist.zip https://github.com/obiba/opal-datasource-spss/releases/download/${SPSS_PLUGIN_VERSION}/opal-datasource-spss-${SPSS_PLUGIN_VERSION}-dist.zip
+
+# Install ReadR datasource plugin
+RUN \
+  curl -L -o $OPAL_DIST/plugins/opal-datasource-readr-${READR_PLUGIN_VERSION}-dist.zip https://github.com/obiba/opal-datasource-readr/releases/download/${READR_PLUGIN_VERSION}/opal-datasource-readr-${READR_PLUGIN_VERSION}-dist.zip
+
+# Install ReadXL datasource plugin
+RUN \
+  curl -L -o $OPAL_DIST/plugins/opal-datasource-readxl-${READXL_PLUGIN_VERSION}-dist.zip https://github.com/obiba/opal-datasource-readxl/releases/download/${READXL_PLUGIN_VERSION}/opal-datasource-readxl-${READXL_PLUGIN_VERSION}-dist.zip
+
+# Install GoogleSheets datasource plugin
+RUN \
+  curl -L -o $OPAL_DIST/plugins/opal-datasource-googlesheets4-${GOOGLESHEETS_PLUGIN_VERSION}-dist.zip https://github.com/obiba/opal-datasource-googlesheets4/releases/download/${GOOGLESHEETS_PLUGIN_VERSION}/opal-datasource-googlesheets4-${GOOGLESHEETS_PLUGIN_VERSION}-dist.zip
+
+# Install Validate analysis plugin
+RUN \
+  curl -L -o $OPAL_DIST/plugins/opal-analysis-validate-${VALIDATE_PLUGIN_VERSION}-dist.zip https://github.com/obiba/opal-analysis-validate/releases/download/${VALIDATE_PLUGIN_VERSION}/opal-analysis-validate-${VALIDATE_PLUGIN_VERSION}-dist.zip
+
+COPY /bin /opt/opal/bin
+COPY /data /opt/opal/data
+RUN chmod +x -R /opt/opal/bin; \
+    chown -R opal /opt/opal; \
+    chmod +x $OPAL_DIST/bin/opal
+
+WORKDIR $OPAL_HOME
 
 VOLUME $OPAL_HOME
 EXPOSE 8080 8443
--- bin/first_run.sh
+++ bin/first_run.sh
@@ -1,23 +1,61 @@
 #!/bin/bash
 
-curl -X POST -k -H "Accept:application/x-protobuf+json" https://localhost:8443/ws/auth/sessions -d "username=administrator" -d "password=$OPAL_ADMINISTRATOR_PASSWORD" --cookie-jar /tmp/.cookie-jar
-
 # Configure some databases for IDs and data
+
+# Legacy parameters
 if [ -n "$MONGO_PORT_27017_TCP_ADDR" ]
-	then
-	echo "Initializing Opal databases with MongoDB..."
-	if [ -z "$MYSQLIDS_PORT_3306_TCP_ADDR" ]
-		then
-		sed s/@mongo_host@/$MONGO_PORT_27017_TCP_ADDR/g /opt/opal/data/mongodb-ids.json | \
-    		sed s/@mongo_port@/$MONGO_PORT_27017_TCP_PORT/g | \
-    		curl -X POST -k -H "Accept:application/x-protobuf+json" -H "Content-Type:application/json" https://localhost:8443/ws/system/databases --cookie /tmp/.cookie-jar -d @-
-    fi
-	sed s/@mongo_host@/$MONGO_PORT_27017_TCP_ADDR/g /opt/opal/data/mongodb-data.json | \
-    	sed s/@mongo_port@/$MONGO_PORT_27017_TCP_PORT/g | \
-    	curl -X POST -k -H "Accept:application/x-protobuf+json" -H "Content-Type:application/json" https://localhost:8443/ws/system/databases --cookie /tmp/.cookie-jar -d @-
+then
+	MONGO_HOST=$MONGO_PORT_27017_TCP_ADDR
+fi
+if [ -n "$MONGO_PORT_27017_TCP_PORT" ]
+then
+        MONGO_PORT=$MONGO_PORT_27017_TCP_PORT
+fi
+if [ -n "$MYSQLIDS_PORT_3306_TCP_ADDR" ]
+then
+        MYSQLIDS_HOST=$MYSQLIDS_PORT_3306_TCP_ADDR
+fi
+if [ -n "$MYSQLIDS_PORT_3306_TCP_PORT" ]
+then
+        MYSQLIDS_PORT=$MYSQLIDS_PORT_3306_TCP_PORT
+fi
+if [ -n "$MYSQLDATA_PORT_3306_TCP_ADDR" ]
+then
+        MYSQLDATA_HOST=$MYSQLDATA_PORT_3306_TCP_ADDR
+fi
+if [ -n "$MYSQLDATA_PORT_3306_TCP_PORT" ]
+then
+        MYSQLDATA_PORT=$MYSQLDATA_PORT_3306_TCP_PORT
 fi
 
-if [ -n "$MYSQLIDS_PORT_3306_TCP_ADDR" ]
+#
+# MongoDB
+#
+
+if [ -n "$MONGO_HOST" ]
+then
+	if [ -z "$MONGO_PORT" ]
+	then
+		MONGO_PORT=27017
+	fi
+	if [ -z "$MYSQLIDS_HOST" ]
+		then
+		echo "Initializing Opal IDs database with MongoDB..."
+		sed s/@mongo_host@/$MONGO_HOST/g /opt/opal/data/mongodb-ids.json | \
+    		sed s/@mongo_port@/$MONGO_PORT/g | \
+    		opal rest -o https://localhost:8443 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m POST /system/databases --content-type "application/json"
+	fi
+	echo "Initializing Opal data database with MongoDB..."
+	sed s/@mongo_host@/$MONGO_HOST/g /opt/opal/data/mongodb-data.json | \
+    	sed s/@mongo_port@/$MONGO_PORT/g | \
+    	opal rest -o https://localhost:8443 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m POST /system/databases --content-type "application/json"
+fi
+
+#
+# MySQL (TODO MariaDB support)
+#
+
+if [ -n "$MYSQLIDS_HOST" ]
 	then
 	echo "Initializing Opal IDs database with MySQL..."
 
@@ -33,15 +71,15 @@ if [ -n "$MYSQLIDS_PORT_3306_TCP_ADDR" ]
 		MID_USER=$MYSQLIDS_USER
 	fi
 
-	sed s/@mysql_host@/$MYSQLIDS_PORT_3306_TCP_ADDR/g /opt/opal/data/mysqldb-ids.json | \
-    	sed s/@mysql_port@/$MYSQLIDS_PORT_3306_TCP_PORT/g | \
+	sed s/@mysql_host@/$MYSQLIDS_HOST/g /opt/opal/data/mysqldb-ids.json | \
+    	sed s/@mysql_port@/$MYSQLIDS_PORT/g | \
     	sed s/@mysql_db@/$MID_DB/g | \
     	sed s/@mysql_user@/$MID_USER/g | \
     	sed s/@mysql_pwd@/$MYSQLIDS_PASSWORD/g | \
-    	curl -X POST -k -H "Accept:application/x-protobuf+json" -H "Content-Type:application/json" https://localhost:8443/ws/system/databases --cookie /tmp/.cookie-jar -d @-
+    	opal rest -o https://localhost:8443 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m POST /system/databases --content-type "application/json"
 fi
 
-if [ -n "$MYSQLDATA_PORT_3306_TCP_ADDR" ]
+if [ -n "$MYSQLDATA_HOST" ]
 	then
 	echo "Initializing Opal data database with MySQL..."
 
@@ -58,23 +96,25 @@ if [ -n "$MYSQLDATA_PORT_3306_TCP_ADDR" ]
 	fi
 
 	MD_DEFAULT="false"
-	if [ -z "$MONGO_PORT_27017_TCP_ADDR" ]
+	if [ -z "$MONGO_HOST" ]
 		then
 		MD_DEFAULT="true"
 	fi
 
-	sed s/@mysql_host@/$MYSQLDATA_PORT_3306_TCP_ADDR/g /opt/opal/data/mysqldb-data.json | \
-    	sed s/@mysql_port@/$MYSQLDATA_PORT_3306_TCP_PORT/g | \
+	sed s/@mysql_host@/$MYSQLDATA_HOST/g /opt/opal/data/mysqldb-data.json | \
+    	sed s/@mysql_port@/$MYSQLDATA_PORT/g | \
     	sed s/@mysql_db@/$MD_DB/g | \
     	sed s/@mysql_user@/$MD_USER/g | \
     	sed s/@mysql_pwd@/$MYSQLDATA_PASSWORD/g | \
     	sed s/@mysql_default@/$MD_DEFAULT/g | \
-    	curl -X POST -k -H "Accept:application/x-protobuf+json" -H "Content-Type:application/json" https://localhost:8443/ws/system/databases --cookie /tmp/.cookie-jar -d @-
+    	opal rest -o https://localhost:8443 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m POST /system/databases --content-type "application/json"
 fi
 
 # Configure datashield packages
-if [ -n "$RSERVER_PORT_6312_TCP_ADDR" ]
+if [ -n "$RSERVER_HOST" ]
 	then
 	echo "Initializing Datashield..."
-	curl -X POST -k -H "Accept:application/x-protobuf+json" -H "Content-Type:application/json" https://localhost:8443/ws/system/databases --cookie /tmp/.cookie-jar -d @-
+	opal rest -o https://localhost:8443 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m POST /datashield/packages?name=datashield
+	opal rest -o https://localhost:8443 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m POST "/service/r/packages?name=obiba%2Fresourcer&ref=master"
 fi
+
