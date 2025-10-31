4368582736cdd0e663db2e64b9c00c6d40230d5e
Yannick Marcon <yannick.marcon@obiba.org>
2025-10-31 12:26:17 +0100

    feat: always set properties

--- bin/start.sh
+++ bin/start.sh
@@ -45,122 +45,108 @@ then
   cp -r /usr/share/opal/plugins/* $OPAL_HOME/plugins
 fi
 
-# check if 1st run. Then configure properties.
-if [ ! -f /opt/opal/bin/first_run.sh.done ]
+# Configure properties
+
+#
+# Opal
+#
+
+if [ -n "$APP_URL" ]
 then
+  set_property "org.obiba.opal.public.url" "$APP_URL" "$OPAL_HOME/conf/opal-config.properties"
+fi
 
-  #
-  # Opal
-  #
+if [ -n "$APP_CONTEXT_PATH" ]
+then
+  set_property "org.obiba.opal.server.context-path" "$APP_CONTEXT_PATH" "$OPAL_HOME/conf/opal-config.properties"
+fi
 
-  if [ -n "$APP_URL" ]
+#
+# Agate
+#
+
+if [ -n "$AGATE_HOST" ]
   then
-    set_property "org.obiba.opal.public.url" "$APP_URL" "$OPAL_HOME/conf/opal-config.properties"
-  fi
-
-  if [ -n "$APP_CONTEXT_PATH" ]
+  echo "Setting Agate connection..."
+  AGATE_URL=$AGATE_HOST
+  if [ -n "$AGATE_PORT" ]
   then
-    set_property "org.obiba.opal.server.context-path" "$APP_CONTEXT_PATH" "$OPAL_HOME/conf/opal-config.properties"
+    AGATE_URL="$AGATE_HOST:$AGATE_PORT"
   fi
-
-  #
-  # Agate
-  #
-
-  if [ -n "$AGATE_HOST" ]
-    then
-    echo "Setting Agate connection..."
-    AGATE_URL=$AGATE_HOST
-    if [ -n "$AGATE_PORT" ]
-    then
-      AGATE_URL="$AGATE_HOST:$AGATE_PORT"
-    fi
-    if [[ ! "$AGATE_URL" =~ ^http ]]
-    then
-      AGATE_URL="https://$AGATE_URL"
-    fi
-    set_property "org.obiba.realm.url" "$AGATE_URL" "$OPAL_HOME/conf/opal-config.properties"
-  elif [ -n "$AGATE_URL" ]
-    then
-    echo "Setting Agate connection..."
-    set_property "org.obiba.realm.url" "$AGATE_URL" "$OPAL_HOME/conf/opal-config.properties"
-  else
-    echo "Disabling default Agate setting as AGATE_HOST is not defined..."
-    set_property "org.obiba.realm.url" "" "$OPAL_HOME/conf/opal-config.properties"
-  fi
-
-  #
-  # Rock R server
-  #
-
-  if [ -n "$ROCK_HOSTS" ]
+  if [[ ! "$AGATE_URL" =~ ^http ]]
   then
-    echo "Setting Rock R server connection..."
-    set_property "apps.discovery.rock.hosts" "$ROCK_HOSTS" "$OPAL_HOME/conf/opal-config.properties"
+    AGATE_URL="https://$AGATE_URL"
   fi
-
-  if [ -n "$ROCK_ADMINISTRATOR_USER" ] && [ -n "$ROCK_ADMINISTRATOR_PASSWORD" ]
+  set_property "org.obiba.realm.url" "$AGATE_URL" "$OPAL_HOME/conf/opal-config.properties"
+elif [ -n "$AGATE_URL" ]
   then
-    echo "Setting Rock R server administrator credentials..."
-    set_property "rock.default.administrator.username" "$ROCK_ADMINISTRATOR_USER" "$OPAL_HOME/conf/opal-config.properties"
-    set_property "rock.default.administrator.password" "$ROCK_ADMINISTRATOR_PASSWORD" "$OPAL_HOME/conf/opal-config.properties"
-  fi
+  echo "Setting Agate connection..."
+  set_property "org.obiba.realm.url" "$AGATE_URL" "$OPAL_HOME/conf/opal-config.properties"
+else
+  echo "Disabling default Agate setting as AGATE_HOST is not defined..."
+  set_property "org.obiba.realm.url" "" "$OPAL_HOME/conf/opal-config.properties"
+fi
 
-  if [ -n "$ROCK_MANAGER_USER" ] && [ -n "$ROCK_MANAGER_PASSWORD" ]
+#
+# Rock R server
+#
+
+if [ -n "$ROCK_HOSTS" ]
+then
+  echo "Setting Rock R server connection..."
+  set_property "apps.discovery.rock.hosts" "$ROCK_HOSTS" "$OPAL_HOME/conf/opal-config.properties"
+fi
+
+if [ -n "$ROCK_ADMINISTRATOR_USER" ] && [ -n "$ROCK_ADMINISTRATOR_PASSWORD" ]
+then
+  echo "Setting Rock R server administrator credentials..."
+  set_property "rock.default.administrator.username" "$ROCK_ADMINISTRATOR_USER" "$OPAL_HOME/conf/opal-config.properties"
+  set_property "rock.default.administrator.password" "$ROCK_ADMINISTRATOR_PASSWORD" "$OPAL_HOME/conf/opal-config.properties"
+fi
+
+if [ -n "$ROCK_MANAGER_USER" ] && [ -n "$ROCK_MANAGER_PASSWORD" ]
+then
+  echo "Setting Rock R server manager credentials..."
+  set_property "rock.default.manager.username" "$ROCK_MANAGER_USER" "$OPAL_HOME/conf/opal-config.properties"
+  set_property "rock.default.manager.password" "$ROCK_MANAGER_PASSWORD" "$OPAL_HOME/conf/opal-config.properties"
+fi
+
+if [ -n "$ROCK_USER_USER" ] && [ -n "$ROCK_USER_PASSWORD" ]
+then
+  echo "Setting Rock R server user credentials..."
+  set_property "rock.default.user.username" "$ROCK_USER_USER" "$OPAL_HOME/conf/opal-config.properties"
+  set_property "rock.default.user.password" "$ROCK_USER_PASSWORD" "$OPAL_HOME/conf/opal-config.properties"
+fi
+
+# Configure Rock pod specifications
+if [ -n "$ROCK_POD_IMAGES_ALLOWED" ]
   then
-    echo "Setting Rock R server manager credentials..."
-    set_property "rock.default.manager.username" "$ROCK_MANAGER_USER" "$OPAL_HOME/conf/opal-config.properties"
-    set_property "rock.default.manager.password" "$ROCK_MANAGER_PASSWORD" "$OPAL_HOME/conf/opal-config.properties"
-  fi
+  echo "Setting Rock pod allowed images..."
+  set_property "pods.rock.images.allowed" "$ROCK_POD_IMAGES_ALLOWED" "$OPAL_HOME/conf/opal-config.properties"
+fi
 
-  if [ -n "$ROCK_USER_USER" ] && [ -n "$ROCK_USER_PASSWORD" ]
+if [ -n "$ROCK_POD_SPECS" ]
   then
-    echo "Setting Rock R server user credentials..."
-    set_property "rock.default.user.username" "$ROCK_USER_USER" "$OPAL_HOME/conf/opal-config.properties"
-    set_property "rock.default.user.password" "$ROCK_USER_PASSWORD" "$OPAL_HOME/conf/opal-config.properties"
-  fi
+  echo "Setting Rock pod specifications..."
+  set_property "pods.rock.specs" "$ROCK_POD_SPECS" "$OPAL_HOME/conf/opal-config.properties"
+fi
 
-  # Configure Rock pod specifications
-  if [ -n "$ROCK_POD_IMAGES_ALLOWED" ]
-    then
-    echo "Setting Rock pod allowed images..."
-    set_property "pods.rock.images.allowed" "$ROCK_POD_IMAGES_ALLOWED" "$OPAL_HOME/conf/opal-config.properties"
-  fi
+#
+# R repositories
+#
 
-  if [ -n "$ROCK_POD_SPECS" ]
-    then
-    echo "Setting Rock pod specifications..."
-    set_property "pods.rock.specs" "$ROCK_POD_SPECS" "$OPAL_HOME/conf/opal-config.properties"
-  fi
+if [ -n "$R_REPOS" ]
+then
+  set_property "org.obiba.opal.r.repos" "$R_REPOS" "$OPAL_HOME/conf/opal-config.properties"
+fi
 
-  #
-  # R repositories
-  #
+#
+# Cross Site Request Forgery (CSRF)
+#
 
-  if [ -n "$R_REPOS" ]
-  then
-    set_property "org.obiba.opal.r.repos" "$R_REPOS" "$OPAL_HOME/conf/opal-config.properties"
-  fi
-
-  #
-  # Cross Site Request Forgery (CSRF)
-  #
-
-  if [ -n "$CSRF_ALLOWED" ]
-  then
-    set_property "csrf.allowed" "$CSRF_ALLOWED" "$OPAL_HOME/conf/opal-config.properties"
-  fi
-
-  #
-  # R server (legacy)
-  #
-
-  if [ -n "$RSERVER_HOST" ]
-  then
-    echo "Setting R server connection..."
-    set_property "org.obiba.opal.Rserve.host" "$RSERVER_HOST" "$OPAL_HOME/conf/opal-config.properties"
-  fi
-
+if [ -n "$CSRF_ALLOWED" ]
+then
+  set_property "csrf.allowed" "$CSRF_ALLOWED" "$OPAL_HOME/conf/opal-config.properties"
 fi
 
 #
